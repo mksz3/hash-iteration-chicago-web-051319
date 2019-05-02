2ae6c444c60c7ffa89a19bddd43be1c8301f0865
Markus Sanders <markus.sanderst@gmail.com>
2019-05-02 21:54:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each {
     |kid, age|
-    if kid[age] <= 12
+    if kid[age] > 12
       puts "Happy Birthday #{kid}! You are now #{age} years old!"
      else 
       puts "You are too old for this."
